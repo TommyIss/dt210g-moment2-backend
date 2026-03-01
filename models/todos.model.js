e947bf9c2648c2e4ec96bf74cdccf053e8c01ba6
Tommy Issa <tommyissa@hotmail.com>
2026-03-01 16:01:12 +0100

    Har skapat model och server-fil

---
+++ models/todos.model.js
@@ -0,0 +1,65 @@
+/** Model för Todo */
+
+async function createTable(mysql) {
+    let sql = `
+        DROP TABLE IF EXISTS todos;
+        CREATE TABLE todos(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            decription TEXT NOT NULL,
+            status ENUM('Ej påbörjad', 'Pågående', 'Avklarad') NOT NULL DEFAULT 'Ej påbörjad',
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        );
+    `;
+
+    let [rows] = await mysql.query(sql);
+
+    return rows;
+}
+
+async function getAllTodos(mysql) {
+    let sql = `SELECT * FROM todos`;
+    let [rows] = await mysql.query(sql);
+
+    return rows;
+}
+
+async function getTodoById(mysql, id) {
+    let sql = `SELECT * FROM todos WHERE id=?`;
+
+    let [rows] = await mysql.query(sql, [id]);
+
+    return rows[0];
+}
+
+async function addTodo(mysql, todo) {
+    let { title, description, status } = todo;
+
+    let sql = `INSERT INTO todos(title, description, status)VALUES(?, ?, ?);`;
+
+    let [result] = await mysql.query(sql, [title, description, status]);
+    
+    return { id: result.insertId, ...todo};
+}
+
+async function updateTodo(mysql, id, todo) {
+    let { title, description, status } = todo;
+
+    let sql = `UPDATE todos SET title=?, description=?, status=? WHERE id=?;`;
+
+    let [result] = await mysql.query(sql, [title, description, status, id]);
+
+    return result;
+    
+}
+
+async function deleteTodo(mysql, id) {
+    let sql = `DELETE FROM todos WHERE id=?`;
+
+    let [result] = await mysql.query(sql, [id]);
+
+    return result;
+}
+
+module.exports = { createTable, getAllTodos, getTodoById, addTodo, updateTodo, deleteTodo };
---
+++ server.js
@@ -0,0 +1,41 @@
+/** Moment 2 för kursen DT210G, Fördjupad frontend utveckling */
+
+let fastify = require('fastify')({
+    logger: true
+});
+let mysql = require('@fastify/mysql');
+require('dotenv').config();
+let cors = require('@fastify/cors');
+let port = process.env.PORT | 3000;
+let host = '0.0.0.0';
+
+// Anslutnings konfigurationer till databasen
+fastify.register(mysql, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    promise: true,
+    multipleStatements: true
+});
+
+fastify.register(cors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+});
+
+// Routes
+fastify.get('/', (req, reply) => {
+    reply.send({message: 'Välkomment till webbtjänst för Moment 2 i kursen DT210G, Fördjupad frontend-utveckling'});
+});
+
+// Starta servern
+fastify.listen({ port: port, host: host }, (err, adress) => {
+    if(err) {
+        fastify.log.error(err);
+        process.exit(1);
+    } else {
+        console.log('Server körs på adress: ' + adress);
+    }
+})
