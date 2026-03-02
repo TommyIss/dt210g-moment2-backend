338a28fc24ba409671670e8ffa0ccdd6e0d1e7f1
Tommy Issa <tommyissa@hotmail.com>
2026-03-02 10:18:48 +0100

    Har ändrat beskrivning så att den kan vara valfri

--- models/todos.model.js
+++ models/todos.model.js
@@ -6,7 +6,7 @@ async function createTable(mysql) {
         CREATE TABLE todos(
             id INT AUTO_INCREMENT PRIMARY KEY,
             title VARCHAR(255) NOT NULL,
-            description TEXT NOT NULL,
+            description TEXT NULL,
             status ENUM('Ej påbörjad', 'Pågående', 'Avklarad') NOT NULL DEFAULT 'Ej påbörjad',
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
